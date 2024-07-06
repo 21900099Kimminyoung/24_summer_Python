666fdc3a702f50dbdcd8e19861798fda973587b9
Minyong Kim <21900099@handong.ac.kr>
2024-07-06 14:20:25 +0900

    소인수 분해하기_time save version

--- 약수/소인수분해.py
+++ 약수/소인수분해.py
@@ -1,13 +1,16 @@
 # n 을 입력받는다.
 # 입력받은 n을 나누었을 때 1이 나올때 까지 반복한다.
-n = int(input())
+import sys
+n = int(sys.stdin.readline().strip())
 i = 2
-while True:
-    if(n%i == 0):
+while i < n:
+    if n % i == 0:
         print(i)
-        if n/i == 1:
-            break
-        n = n/i
-        i = 2
+        n //= i
     else:
-        i = i+1
+        if i == 2:
+            i += 1
+        else:
+            i += 2
+if n > 1:
+    print(n)
