8a5023f9d3381df60460a2dd914359db152f2a38
Minyong Kim <21900099@handong.ac.kr>
2024-07-11 23:32:24 +0900

    분해합_너무 복작하게 생각하지 않기

--- 브루트포스/분해합.py
+++ 브루트포스/분해합.py
@@ -3,13 +3,9 @@
 # 계속해서 sum과 temp를 더해서 N 인지 확인
 N = int(input())
 for i in range(1,N+1):
-    s = str(i)
-    temp = i
-    sum = 0
-    for j in range(len(s)-1):
-        k = int(i/(10**(j+1)))
-        sum = sum + k
-        temp = temp - k*(10**(j+1))
-    if(sum + temp + i == N):
+    num = sum(map(int,str(i)))
+    if(i + num == N):
         print(i)
         break
+    if(N == i):
+        print(0)
