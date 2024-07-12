86af82e7108113ea4b3ed4e16b0f2f86c7d8a9a3
Minyong Kim <21900099@handong.ac.kr>
2024-07-12 21:18:34 +0900

    메모리 절약, 숫자의 수의 숫자의 범위보다 클때

--- 정렬/수정렬하기3.py
+++ 정렬/수정렬하기3.py
@@ -1,5 +1,9 @@
 import sys
 N = int(input())
-result = []
+result = [0]*10000
 for i in range(N):
-    
+    n = int(sys.stdin.readline().strip())
+    result[n-1] += 1
+for i in range(10000):
+    for j in range(result[i]):
+        print(i+1)
