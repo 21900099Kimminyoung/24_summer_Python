cc5be12ed7cfba29c70a31620e969e05ac6c9b03
Minyong Kim <21900099@handong.ac.kr>
2024-07-13 20:28:52 +0900

    dict, c++의 해쉬와 비슷, id로 접근

---
+++ 정렬/좌표압축.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline().strip())
+X = list(map(int,sys.stdin.readline().split()))
+X1 = sorted(X)
+X1 = dict.fromkeys(X1)
+con = 0
+for i in X1:
+    X1[i] = con
+    con+=1
+for i in range(n):
+    X[i] = X1[X[i]]
+print(*X)
