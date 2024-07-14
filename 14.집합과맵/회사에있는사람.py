8283549ad6d7680cded04c022073c526be053c86
Minyong Kim <21900099@handong.ac.kr>
2024-07-14 17:25:11 +0900

    list보다 dict가 가볍다. dict = {}으로 선언

--- 14.집합과맵/회사에있는사람.py
+++ 14.집합과맵/회사에있는사람.py
@@ -1,5 +1,13 @@
+#list 보다 dict가 가볍다..
 import sys
 n = int(sys.stdin.readline().strip())
-name = []
+names = {}
 for i in range(n):
-    
+    name, record = sys.stdin.readline().split()
+    if(record == 'enter'):
+        names[name] = True
+    else:
+        del names[name]
+names = list(names)
+names.sort(reverse=True)
+print(*names,sep='\n')
