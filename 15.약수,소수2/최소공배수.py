7a2001e94fb0c35b3cba948a9a12ecd79b643cf6
Minyong Kim <21900099@handong.ac.kr>
2024-07-17 23:11:38 +0900

    최소공배수 구하기 출처

--- 15.약수,소수2/최소공배수.py
+++ 15.약수,소수2/최소공배수.py
@@ -29,6 +29,7 @@
 #     print(result)
 
 # math에 함수를 가져와서 사용
+# https://codingpractices.tistory.com/entry/Python-%EC%B5%9C%EC%86%8C%EA%B3%B5%EB%B0%B0%EC%88%98-%EC%B5%9C%EB%8C%80%EA%B3%B5%EC%95%BD%EC%88%98%EB%9E%80-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EC%89%BD%EA%B2%8C-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0
 # 최소공배수 : math.lcm()
 # 최대공약수 : math.gcd()
 import sys
