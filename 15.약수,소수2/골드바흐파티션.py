91ed5efe9f74e5593c7a989391be40d1bb258bc7
Minyong Kim <21900099@handong.ac.kr>
2024-07-20 16:30:55 +0900

    시간 초과 지속적인 발생, 시간 절약 방법 : 알고리즘 사용

---
+++ 15.약수,소수2/골드바흐파티션.py
@@ -0,0 +1,26 @@
+# 첫째 줄에 테스트 케이스의 개수 T (1 ≤ T ≤ 100)가 주어진다. 
+# 각 테스트 케이스는 한 줄로 이루어져 있고, 정수 N은 짝수이고, 
+# 2 < N ≤ 1,000,000을 만족한다.
+# 500,001 까지 소수 인지 미리 계산한다
+# 첫 수를 2로 잡은 경우 N-2 가 소수인지 확인 등등
+import sys
+import math
+num = 1000001
+prime = [False, False] + [True] * 999999
+T = int(sys.stdin.readline().strip())
+
+for i in range(2,num):
+    if prime[i]:
+        for i in range(i*2,num,i):
+            prime[i] = False
+
+for i in range(T):
+    count = 0
+    n = int(sys.stdin.readline().strip())
+    for j in range(2,n//2+1):
+        if prime[j] and prime[n-j]:
+            count += 1
+    print(count)
+
+
+
---
+++ 15.약수,소수2/창문닫기.py
@@ -0,0 +1,5 @@
+# 첫 번째 줄에는 창문의 개수와 사람의 수 N(1 ≤ N ≤ 2,100,000,000)이 주어진다.
+import sys
+n = int(sys.stdin.readline().strip())
+window = [1] * n
+for 
